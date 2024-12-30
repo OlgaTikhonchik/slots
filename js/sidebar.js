6db2cda003856a450cae8d0b7f230e77e78aff00
OlgaTikhonchik <olga882011@gmail.com>
2024-12-31 01:37:27 +0200

    Стилизация

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,18 +1,18 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   const button = document.querySelector(".sidebar__desk");
-//   const list = document.querySelector(".sidebar__list");
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.querySelector(".sidebar__links-button");
+  const list = document.querySelector(".sidebar__links-list");
 
-//   button.addEventListener("click", function () {
-//     const isActive = list.classList.contains("active");
-//     list.classList.toggle("active", !isActive);
-//     button.classList.toggle("active", !isActive);
-//   });
-// });
+  button.addEventListener("click", function () {
+    const isActive = list.classList.contains("active");
+    list.classList.toggle("active", !isActive);
+    button.classList.toggle("active", !isActive);
+  });
+});
 
 // menu__review-content
 
 document.addEventListener("DOMContentLoaded", function () {
-  const button = document.querySelector(".page-nav-desk");
+  const button = document.querySelector(".page-nav-button");
   const list = document.querySelector(".page-nav-list");
 
   button.addEventListener("click", function () {
