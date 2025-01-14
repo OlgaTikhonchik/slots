977b2d0987d17aa4d60a1acc060d06dcb7561fb3
OlgaTikhonchik <olga882011@gmail.com>
2025-01-14 20:05:43 +0200

    Обновления

---
+++ js/anchor.js
@@ -0,0 +1,31 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault(); 
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        
+        if (targetElement) {
+            const offset = 0; 
+            const elementPosition = targetElement.offsetTop;
+            const offsetPosition = elementPosition - offset;
+
+            window.scrollTo({
+                top: offsetPosition,
+                behavior: 'smooth'
+            });
+        }
+       
+    });
+});
+
+// document.addEventListener('DOMContentLoaded', function () {
+
+//     const scroll = new SmoothScroll('.scroll', {
+//         offset: 100, 
+//         speed: 500, 
+//         speedAsDuration: true 
+//     });
+//   });
+  
