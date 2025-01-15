1a4a67015f9ad47017570ba2094f677ad5408234
OlgaTikhonchik <olga882011@gmail.com>
2025-01-15 20:30:01 +0200

    Добавление плавности

--- js/anchor.js
+++ js/anchor.js
@@ -1,31 +1,27 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault(); 
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
 
-        const targetId = this.getAttribute('href').substring(1);
-        const targetElement = document.getElementById(targetId);
+    const targetId = this.getAttribute("href").substring(1);
+    const targetElement = document.getElementById(targetId);
 
-        
-        if (targetElement) {
-            const offset = 0; 
-            const elementPosition = targetElement.offsetTop;
-            const offsetPosition = elementPosition - offset;
+    if (targetElement) {
+      const offset = 0;
+      const elementPosition = targetElement.offsetTop;
+      const offsetPosition = elementPosition - offset;
 
-            window.scrollTo({
-                top: offsetPosition,
-                behavior: 'smooth'
-            });
-        }
-       
-    });
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: "smooth",
+      });
+    }
+  });
 });
 
-// document.addEventListener('DOMContentLoaded', function () {
-
-//     const scroll = new SmoothScroll('.scroll', {
-//         offset: 100, 
-//         speed: 500, 
-//         speedAsDuration: true 
-//     });
+// document.addEventListener("DOMContentLoaded", function () {
+//   const scroll = new SmoothScroll(".scroll", {
+//     offset: 100,
+//     speed: 100,
+//     speedAsDuration: true,
 //   });
-  
+// });
